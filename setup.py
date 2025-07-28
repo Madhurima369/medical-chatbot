35f2200b7935137d80136eee4ba264bc2fbdbaee
MadhuRima72 <madhurima.sit@mohs10.io>
2025-07-28 12:55:11 +0530

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Experimental Project 1',
+    version='0.0.0',
+    author='Madhurima Sit',
+    author_email='sitmadhurima@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
